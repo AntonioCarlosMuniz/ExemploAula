d598c76866414ddcb83e16e59301de98726de66e
anton <anton@AntonioCarlos>
2017-11-06 18:02:50 -0200

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (mes)
+            {
+                case "Janeiro":
+                case "Março":
+                case "Maio":
+                case "Julho":
+                case "Agosto":
+                case "Outubro":
+                case "Dezembro":
+                    Console.WriteLine("Este mês tem 31 dias");
+                    break;
+                case "Fevereiro":
+                    Console.WriteLine("Este mês tem 28 ou 29 dias");
+                    break;
+                default:
+                    Console.WriteLine("Este mês tem 30 dias");
+                    break;
+            }
+        }
+    }
+}
